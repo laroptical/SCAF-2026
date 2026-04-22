8c189869079e8a94fb72731a3bd8dd4eebcf90dd
Larhlimi <larhlimi@gmail.com>
2026-04-21 21:12:25 -0400

    Create improved configuration file

---
+++ scaf_ls/config_fixed.py
@@ -0,0 +1,8 @@
+# Configuration settings for SCAF
+
+# Example updated configurations
+config = {
+    'purge_gap': 10,
+    'macro_features': ['Fed rate', 'VIX', 'Sentiment'],
+    'data_loader': 'MultiAssetLoader',
+}
